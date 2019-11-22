798d2ce1ecbd786d47fa5461f1749c636c7cc34e
tramary <kagaminerin.zyasinroki@aol.jp>
2019-11-22 12:18:07 +0900

    コンパイルに必要なファイル追加

--- contents.js
+++ contents.js
@@ -3,10 +3,10 @@ chrome.runtime.onMessage.addListener(function (request, sender, callback) {
     window.location.href = request.url;
 });
 document.addEventListener('click', function () {
-    let el = event.target;
-    let elp = el.parentElement;
-    let lsaki = el.getAttribute("href");
-    let ltxt = el.innerHTML;
+    var el = event.target;
+    var elp = el.parentElement;
+    var lsaki = el.getAttribute("href");
+    var ltxt = el.innerHTML;
     chrome.runtime.sendMessage({ "lsaki": lsaki, "ltxt": ltxt, "basedom": this.domain, "type": "main" }, function (response) {
         //ここに書いた処理はどうやら元のタブにて適用されるらしい　それもそうか。
     });
